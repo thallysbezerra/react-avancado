5af86a9d2bd07c5d4dbf924a122e15f57cad9579
Thallys Bezerra <bezerra.thallys@gmail.com>
2020-08-04 00:06:34 -0300

    add Jest configuration

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  testEnvironment: 'jsdom', // Simulate browser
+  testPathIgnorePatterns: ['/node_modules', '/.next/'], // Ignore some directories
+  collectCoverage: true, // Test coverage
+  collectCoverageFrom: ['src/**/*.ts(x)'], // Test coverage from
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'] // Load config before running test
+}
