c078cd5716c43f3dc71e3df677f22a32f6bcea6f
Thallys Bezerra <bezerra.thallys@gmail.com>
2020-08-25 01:08:06 -0300

    fix default title

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "stories": [
+    "../src/components/**/stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-essentials",
+    "@storybook/addon-links",
+    "@storybook/addon-knobs",
+    "@storybook/preset-typescript"
+  ]
+}
