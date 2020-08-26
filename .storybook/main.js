8100b80b1d6ffe6b949d1407cfe5f913763aa8ec
Thallys Bezerra <bezerra.thallys@gmail.com>
2020-08-26 02:14:12 -0300

    storybook control config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,4 @@
 module.exports = {
-  "stories": [
-    "../src/components/**/stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-essentials",
-    "@storybook/addon-links",
-    "@storybook/addon-knobs",
-    "@storybook/preset-typescript"
-  ]
+  "stories": ["../src/components/**/stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-essentials"]
 }
--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,19 +1,19 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import { withKnobs, text } from '@storybook/addon-knobs'
 import Main from '.'
 
 export default {
   title: 'Main',
   component: Main,
-  decorators: [withKnobs]
+  args: {
+    title: 'Default title',
+    description: 'Default description'
+  }
 } as Meta
 
-export const Basic: Story = () => (
-  <Main
-    description={text(
-      'Description',
-      'TypeScript, ReactJS, NextJS e Styled Components'
-    )}
-    title={text('Title', 'React Avançado!')}
-  />
-)
+export const Basic: Story = (args) => <Main {...args} />
+Basic.args = {
+  title: 'Basic title',
+  description: 'Basic description'
+}
+
+export const Default: Story = (args) => <Main {...args} />
