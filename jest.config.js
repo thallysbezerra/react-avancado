8dd4ed1a9fd667a7ff826094047ec3a102e2d599
Thallys Bezerra <bezerra.thallys@gmail.com>
2020-08-28 01:35:13 -0300

    fix regex in collectCoverageFrom

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   testEnvironment: 'jsdom', // Simulate browser
   testPathIgnorePatterns: ['/node_modules', '/.next/'], // Ignore some directories
   collectCoverage: true, // Test coverage
-  collectCoverageFrom: ['src/**/*.ts(x)'], // Test coverage from
+  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'], // Test coverage from
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'] // Load config before running test
 }
